1dff605bde56cb20a0fae3f9582e8d93f783ebfd
syswraith <syswraith@proton.me>
2024-11-03 01:42:08 +0530

    return 0

--- 004_arrays/intro_array.c
+++ 004_arrays/intro_array.c
@@ -6,4 +6,5 @@ int main()
     int arr2[5] = {1,2,3,4,5};
     printf("Size of without specifying array size: %lu bytes\nSize with specifying array size: %lu bytes\n", sizeof(arr1), sizeof(arr2));
     printf("Number of elements in arr1: %d\nNumber of elements in arr2: %d\n", (sizeof(arr1)/sizeof(arr1[0])), (sizeof(arr2)/sizeof(arr2[0])));
+    return 0;
 }
