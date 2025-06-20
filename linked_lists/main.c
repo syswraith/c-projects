316e2af580cb13ca12ccb9c182f32969b5fd7454
syswraith <syswraith@proton.me>
2025-06-20 12:08:28 +0530

    gitignore

--- linked_lists/main.c
+++ linked_lists/main.c
@@ -1,11 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// create a Node datatype and make it have a data and node* pointer
-//
-typedef struct Node
-{
-    int data;
-    struct Node* next;
-} Node;
 
+typedef struct Node {
+    int data;
+    struct Node *next;
+} node_t;
+
+
+node_t* createNode(int value)
+{
+    node_t* newNode = (node_t*) malloc(sizeof(node_t));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+
+int main() 
+{
+    node_t* n1;
+    node_t* n2;
+    node_t* n3;
+    n1 = createNode(1);
+    n2 = createNode(2);
+    n3 = createNode(3);
+
+    n1->next = n2;
+    n2->next = n3;
+    n3->next = NULL;
+
+    node_t* temp = n1;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
