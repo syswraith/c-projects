10703a7865920c11ea33870fc965991322e0b4f4
syswraith <syswraith@proton.me>
2025-09-06 02:13:43 +0530

    Sat Sep  6 02:11:25 AM IST 2025

--- allocation/main.c
+++ allocation/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
 
 int main()
 {
-  int *arr = malloc(sizeof(int) * 100);
-  for (int i = 0; i < sizeof(arr)/sizeof(&arr[0]); i++)
-    printf("%d ", arr[i]);
+  int counter = 5;
+  char *arr = calloc(counter, sizeof(char));
+  int charcount = 0;
+
+  while (true)
+  {
+
+    char input = getchar();
+
+    if (isspace(input) || input == EOF) {
+      break;
+    } 
+    else {
+      if (charcount >= counter) {
+        counter += 5;
+        arr = realloc(arr, sizeof(char)*counter);
+        arr[charcount] = input;
+      }
+      else {
+        arr[charcount] = input;
+      }
+      charcount++;
+    }
+
+  }
+
+  for (int i = 0; i < counter; i++) printf("%c", arr[i]);
   printf("\n");
+
+  free(arr);
+  arr = NULL;
 
   return 0;
 }
